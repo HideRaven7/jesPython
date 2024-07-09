e706389811008427b421823b55d016b2c22c7139
Smailyn <smaburgos2003@gmail.com>
2024-07-09 16:46:16 -0400

    casi terminado

--- app.py
+++ app.py
@@ -78,13 +78,51 @@ def p_perfil():
 def p_refuerzo_libros():
     return render_template('./profesor/p-refuerzo-libros.html')
 
-@app.route('/profesor/refuerzo/libro/fuente Abriir/')
+@app.route('/profesor/refuerzo/libro/fuenteAbriir/')
 def p_libro_refuerzo():
     return render_template('./profesor/p-libro-refuerzo.html')
 
 @app.route('/profesor/refuerzo/videos')
 def p_refuerzo_videos():
     return render_template('./profesor/p-refuerzo-videos.html')
+
+@app.route('/profesor/agregarVideo')
+def p_agregar():
+    return render_template('/profesor/p-agregar-video.html')
+
+@app.route('profespr/materialEstudio')
+def p_material_estudio():
+    return render_template('/profesor/p-material_estudio.html')
+
+@app.route('/profesor/agregarMaterial')
+def p_agregar_material():
+    return render_template('/profesor/p-agregar-material.html')
+
+
+@app.route('/profesor/recuerdoEstudio')
+def p_recurso_estudio():
+    return render_template('/profesor/p-recurso_estudio.html')
+
+@app.route('/profesor/materialSubido')
+def p_material_de_curso_subido():
+    return render_template('/profesor/p-material-de-curso-subido.html')
+
+@app.route('/profesor/clasesEnviadas')
+def p_clases_enviadas():
+    return render_template('/profesor/p-clases-enviada.html')
+
+@app.route('/profesor/tareaEstudiante')
+def p_tarea_e():
+    return render_template('/profesor/p-tarea-e.html')
+
+@app.route('/profesor/reporte')
+def p_report_a():
+    return render_template('/profesor/p-report-a.html')
+
+@app.route('/profesor/perfilE')
+def p_perfil_e():
+    return render_template('/profesor/p-perfil-e.html')
+
 
 if __name__ == '__main__':
     app.run(port = 3000, debug=True)
